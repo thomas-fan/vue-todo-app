fece8120076e408099210454be82e5dab17ddc54
thomas-fan <youxiboy007@vip.qq.com>
2019-08-01 13:58:01 +0800

    初始化项目，并且完成todolist静态功能

---
+++ src/components/TodoList.vue
@@ -0,0 +1,141 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" placeholder="需要做的事情"
+           v-model="newTodo" @keyup.enter="addTodo"
+    >
+    <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+      <div class="todo-item-left">
+        <div v-if="!todo.editing" class="todo-item-label" @dblclick="editTodo(todo)"> {{todo.title}}</div>
+        <input v-if="todo.editing" @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)" @blur="doneEdit(todo)"
+               class="todo-item-edit"
+               type="text" v-model="todo.title" v-focus>
+      </div>
+      <div class="remove-item" @click="removeTodo(index)">
+        &times;
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "TodoList",
+        data() {
+            return {
+                newTodo: '',
+                idForTodo: 3,
+                beforeEditCache: '',
+                todos: [
+                    {
+                        'id': 1,
+                        'title': '学习vue',
+                        'completed': false,
+                        'editing': false,
+                    },
+                    {
+                        'id': 2,
+                        'title': '学习react',
+                        'completed': false,
+                        'editing': false,
+
+                    }
+
+
+                ]
+            }
+        },
+        directives: {
+            focus: {
+                inserted: function (el) {
+                    el.focus()
+                }
+            }
+        },
+        methods: {
+            addTodo() {
+                if (this.newTodo.trim().length === 0) {
+                    return
+                }
+                this.todos.push({
+                    id: this.idForTodo,
+                    title: this.newTodo,
+                    completed: false
+                })
+                this.newTodo = ''
+                this.idForTodo++
+            },
+            removeTodo(index) {
+                this.todos.splice(index, 1)
+            },
+
+            editTodo(todo) {
+                this.beforeEditCache = todo.title
+                todo.editing = true
+            },
+            doneEdit(todo) {
+                if (todo.title.trim().length === 0) {
+                    todo.title = this.beforeEditCache
+                }
+                todo.editing = false
+            },
+            cancelEdit(todo) {
+                todo.title = this.beforeEditCache
+                todo.editing = false
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+
+    &:focus {
+      outline: 0;
+    }
+  }
+
+  .todo-item {
+    margin-bottom: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .remove-item {
+    cursor: pointer;
+    margin-left: 14px;
+
+    &:hover {
+      color: black;
+    }
+  }
+
+  .todo-item-left {
+    display: flex;
+    align-items: center;
+  }
+
+  .todo-item-label {
+    padding: 10px;
+    border: 1px solid #fff;
+    margin-left: 12px;
+  }
+
+  .todo-item-edit {
+    font-size: 24px;
+    color: #2c3e50;
+    margin-left: 12px;
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+
+    &:focus {
+      outline: none;
+    }
+  }
+</style>
